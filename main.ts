e54a5a419f04beee13dfa8e9c3cabf0cc3760996
person4202 <l31raya@allsaints.wa.edu.au>
2025-09-16 18:49:07 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,22 @@
 //% weight=100 color=#ff8800 icon="\uf0a4"
-//% block="tileBelowSprite"
+//% block="Tile-below-sprite"
 namespace tileBelowSprite {
     /**
      * Get the tile location directly below a sprite
      */
     //% block="tile below %sprite=variables_get(mySprite)"
     //% sprite.shadow=variables_get
-    export function tileBelow(sprite: Sprite): tiles.Location {
+    export function tileBelowSprite(sprite: Sprite): tiles.Location {
         let col = Math.idiv(sprite.x, 16)
         let row = Math.idiv(sprite.y, 16) + 1
         return tiles.getTileLocation(col, row)
+    }
+
+    /**
+     * Get the tile location directly below another tile location
+     */
+    //% block="tile below location %loc"
+    export function tileBelowLocation(loc: tiles.Location): tiles.Location {
+        return tiles.getTileLocation(loc.col, loc.row + 1)
     }
 }
